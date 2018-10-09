b5e860ee5cff29e64cb454d09a7fd38e885e5e0b
Tim Woodacre <phobosanomaly37@gmail.com>
2018-10-08 23:36:52 -0600

    Update to Query 1

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,6 +1,22 @@
+USE firstDatabase
+
 SELECT Name, count(Province) AS city_count
 FROM City
 WHERE Country = 'USA'
 GROUP BY Name
-HAVING COUNT(Province) >1
+HAVING count(Province) >1
 ORDER BY Name desc;
+
+
+SELECT Mountains, count(Mountains) AS "Mountain Count"
+FROM dbo.Mountain
+GROUP BY Mountains
+HAVING count(Mountains) >=1
+ORDER BY "Mountain Count" desc;
+GO
+
+
+SELECT *
+FROM dbo.Island
+ORDER BY Area desc;
+GO
